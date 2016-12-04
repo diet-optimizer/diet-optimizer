125c36721d8f772052aa656437e8b8464569a95c
Nilufer Polat <niluferplt@gmail.com>
2016-12-03 20:12:32 -0500

    deleted something i tried

--- diet_optimizer/settings.py
+++ diet_optimizer/settings.py
@@ -11,33 +11,3 @@ load_dotenv(dotenv_path)
 SPOONACULAR_KEY = os.environ.get("SPOONACULAR_KEY")
 APP_KEY = os.environ.get("APP_KEY")
 
-import sys
-import urlparse
-try:
-
-    # Check to make sure DATABASES is set in settings.py file.
-    # If not default to {}
-
-    if 'DATABASES' not in locals():
-        DATABASES = {}
-
-    if 'DATABASE_URL' in os.environ:
-        url = urlparse.urlparse(os.environ['DATABASE_URL'])
-
-        # Ensure default database exists.
-        DATABASES['default'] = DATABASES.get('default', {})
-
-        # Update with environment configuration.
-        DATABASES['default'].update({
-            'NAME': url.path[1:],
-            'USER': url.username,
-            'PASSWORD': url.password,
-            'HOST': url.hostname,
-            'PORT': url.port,
-        })
-
-
-        if url.scheme == 'mysql':
-            DATABASES['default']['ENGINE'] = 'flask.db.backends.mysql'
-except Exception:
-    print 'Unexpected error:', sys.exc_info()
