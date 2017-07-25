2c8d2ae26d0971910a6a4a040409fe488d25b692
Rachel BLIN <Rachel BLIN>
2017-07-25 13:13:57 -0500

    creation of the branch

--- runserver.py
+++ runserver.py
@@ -1,6 +1,8 @@
 from diet_optimizer import app
 import os
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 if __name__ == '__main__':
     # app.run(threaded=True, debug=True)
     port = int(os.environ.get("PORT", 5000))
